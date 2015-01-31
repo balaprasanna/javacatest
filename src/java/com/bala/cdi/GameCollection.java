fedb734cd48c43d1441cdd55f43e6bc93b2d629d
balaprasanna <balaprasannav2009@gmail.com>
2015-02-01 01:26:53 +0800

    test commit

---
+++ src/java/com/bala/cdi/GameCollection.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bala.cdi;
+
+import java.util.HashMap;
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GameCollection {
+private HashMap<String,Game> listOfGames = new HashMap<String,Game>();
+
+    public HashMap<String, Game> getListOfGames() {
+        return listOfGames;
+    }
+    
+    public Game getAGame(String name){
+        return ((listOfGames.get(name))!=null? listOfGames.get(name):null);
+    }
+    
+    public void addGame(String name,Game g){
+       listOfGames.put(name, g);
+    }
+    
+    public  void RemoveGame(String name){
+        listOfGames.remove(name);
+    }
+    
+}
